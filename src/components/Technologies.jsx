d25fae93ac7b592194d18e85fd5fd2f50cbd4753
teosmilevski <teosmile07@gmail.com>
2024-03-29 15:26:52 +0100

    Project setup for work

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,6 +1,9 @@
 export default function Technologies() {
   return (
     <section id="technologies">
+      {/* TODO: MAKE CHANGES HERE 
+      PUT 5 ICONS
+      CHANGE COLOR - PURPLE??? */}
       <h1>aaaa</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis,
