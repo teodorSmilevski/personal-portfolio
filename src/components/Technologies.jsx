d76c57f536ef299ca296d58d57fda63a14569db0
teosmilevski <teosmile07@gmail.com>
2024-03-30 20:15:44 +0100

    updated technologies part

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,11 +1,19 @@
+import htmlLogo from "../assets/html.png";
+import cssLogo from "../assets/css.png";
+import jsLogo from "../assets/js.png";
+import reactLogo from "../assets/react.png";
+import gitLogo from "../assets/git.png";
 export default function Technologies() {
   return (
     <section id="technologies">
       {/* TODO: MAKE CHANGES HERE 
       PUT 5 ICONS
       CHANGE COLOR - PURPLE??? */}
-      <h1>aaaa</h1>
-      <img src="" alt="" />
+      <img src={htmlLogo} alt="html ogo" />
+      <img src={cssLogo} alt="css logo" />
+      <img src={jsLogo} alt="js logo" />
+      <img src={reactLogo} alt="react logo" />
+      <img src={gitLogo} alt="git logo" />
     </section>
   );
 }
