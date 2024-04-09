2408b12993b765499176b62080e520e29c581e33
teodorSmilevski <teosmilevski@outlook.com>
2024-04-09 22:00:18 +0200

    Changed project card template

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,8 @@ import project3Img from "../assets/project-image-example-3.png";
 export default function Projects() {
   const descriptions = [
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, natus? Laboriosam a eius sequi sint praesentium fugiat quo, soluta reiciendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, natus? Laboriosam a eius sequi sint praesentium fugiat quo, soluta reiciendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, natus?",
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit ipsa quae dolorem fugit incidunt aut maiores distinctio cum reiciendis consequuntur esse eos nisi repellendus veritatis, quos ducimus aliquam. Ipsum, officiis velit quasi excepturi cum obcaecati aliquid id qui. ",
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit ipsa quae dolorem fugit incidunt aut maiores distinctio cum reiciendis consequuntur esse eos nisi repellendus veritatis, quos ducimus aliquam. Ipsum, officiis velit quasi excepturi cum obcaecati aliquid id qui. quos ducimus aliquam. Ipsum, officiis velit quasi excepturi cum obcaecati aliquid id qui ",
   ];
 
   return (
@@ -19,14 +21,14 @@ export default function Projects() {
       <ProjectCard
         image={project2Img}
         title="Project2"
-        description={descriptions[0]}
+        description={descriptions[1]}
         githubLink="link"
         liveLink="link"
       />
       <ProjectCard
         image={project3Img}
         title="Project3"
-        description={descriptions[0]}
+        description={descriptions[2]}
         githubLink="link"
         liveLink="link"
       />
