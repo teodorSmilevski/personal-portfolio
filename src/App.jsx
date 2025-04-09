2527d4132b1ebaea451e91e5bdf25b8a41ce46d4
teodorSmilevski <teosmilevski@outlook.com>
2025-04-09 14:34:21 +0200

    Closing the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import "./App.css";
 import Header from "./components/Header";
-import Technologies from "./components/Technologies";
-import AboutMe from "./components/AboutMe";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       <Header />
-      <Technologies />
-      <AboutMe />
-      <Projects />
-      <Footer />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,34 @@
 import NavBar from "./NavBar";
 
-import profilePic from "../assets/personal-picture-NOBG.png";
-
 export default function Header() {
   return (
     <>
       <NavBar />
       <main id="main-header">
         <div className="header-content container">
-          <img src={profilePic} alt="teodor smilevski" />
           <div className="main-text-content">
-            <h1>
-              Frontend <span id="react-word">React</span> Developer 👋
-            </h1>
-            <p>
-              Hi! Im Teodor Smilevski, a passionate frontend developer skilled
-              in creating interactive and user-friendly web experiences
+            <h1>Hello there 👋</h1>
+            <p>I am Teodor, a Web Developer looking for new experiences.</p>
+            <p className="construction-note">
+              🚧 This site is currently under construction. In the meantime,
+              feel free to check out my github and linkedin.
+              <div className="link-logos">
+                <a
+                  href="https://www.linkedin.com/in/teodor-smilevski-21860221b/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bx bxl-linkedin-square bx-md icon-w"></i>
+                </a>
+                <a
+                  href="https://github.com/teodorSmilevski"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bx bxl-github bx-md icon-w"></i>
+                </a>
+              </div>
             </p>
-            <div className="link-logos">
-              <a
-                href="https://www.linkedin.com/in/teodor-smilevski-21860221b/"
-                target="_blank"
-              >
-                <i className="bx bxl-linkedin-square bx-md icon-w"></i>
-              </a>
-              <a href="https://github.com/teodorSmilevski" target="_blank">
-                <i className="bx bxl-github bx-md icon-w"></i>
-              </a>
-              <a
-                href="/TeodorSmilevski_CV_EN.pdf"
-                download="TeodorSmilevski_CV_EN.pdf"
-              >
-                <button className="download-cv">Download CV</button>
-              </a>
-            </div>
           </div>
         </div>
       </main>
